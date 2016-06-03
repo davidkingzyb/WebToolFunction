de30ab06f03f15388517a148e6ad1c874638efb7
DKZ <davidkingzyb@163.com>
2016-06-03 14:36:26 +0800

    mobilecss

--- terminal.js
+++ terminal.js
@@ -1,4 +1,3 @@
-
 /*
 
 license:MIT
@@ -34,8 +33,8 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 github: https://github.com/davidkingzyb/WebToolFunction
 */
 var terminal_alerttimer;
-var terminal=(function(){
-    var csstemplate=`
+var terminal = (function() {
+    var csstemplate = `
 <style>
 #terminal pre{
     margin:0;
@@ -88,25 +87,123 @@ margin-right:10px;
     top: 174px;
     background-color: rgba(0,0,0,0.5);
 }
+
+.terminal_alert{
+font-size:20px;
+color:white;
+background:black;
+border-radius:5px;
+padding:10px;opacity:0.8;
+position:relative;
+float:left;
+right:50%;
+}
+
+#terminal_alertcon{
+    position:fixed;
+    float:left;
+    clear:left;
+    top:45%;
+    left:50%
+
+}
 </style>
     `
-    var template='<div id="terminal" style="display:none;"><div id="terminalbg"></div><pre><pre id="terminal_show">WebToolFunction by DKZ\n</pre>-<input type="text" id="terminal_input" size="50"></pre></div>'
-    function terminal(){
+    var cssmobiletemplate = `
+<style>
+#terminal pre{
+    margin:0;
+}
+#terminal{
+    color:white;
+    font-family: 'Lucida Console', Monaco, monospace;
+    position:absolute;
+    width:100%;
+    height:500px;
+    left:0px;
+    bottom:0px;
+    background-color: rgba(10,10,10,0.3);
+    z-index:999;
+    font-size:30px;
+}
+#terminal>pre{
+position:absolute;
+bottom:10px;
+margin-left: 10px;
+margin-right:10px;
+    
+}
+#terminal_show{
+    overflow-y:scroll;
+    overflow-x:hidden;
+    height: 447px;
+    width: 100%;
+}
+#terminal pre a{
+    color:white;
+}
+#terminal pre a:hover{
+    color:#222;
+}
+#terminal_input{
+    margin:0px;
+    padding:0px;
+    background-color: rgba(10,10,10,0.01);
+    border:0px;
+    color:white;
+    font-family: 'Lucida Console', Monaco, monospace;
+    outline:none;
+    font-size:30px;
+
+}
+#terminalbg{
+    position: relative;
+    height: 50px;
+    width: 100%;
+    top: 450px;
+    background-color: rgba(0,0,0,0.5);
+}
+.terminal_alert{
+font-size:40px;
+color:white;
+background:black;
+border-radius:5px;
+padding:10px;opacity:0.8;
+position:relative;
+float:left;
+right:50%;
+}
+
+#terminal_alertcon{
+    position:fixed;
+    float:left;
+    clear:left;
+    top:45%;
+    left:50%
+
+}
+</style>
+    `
+    var template = '<div id="terminal" style="display:none;"><div id="terminalbg"></div><pre><pre id="terminal_show">WebToolFunction by DKZ\n</pre>-<input type="text" id="terminal_input" size="50"></pre></div>'
+
+    function terminal() {
 
     }
 
-    var class2type = {} ;
-    "Boolean Number String Function Array Date RegExp Object Error".split(" ").forEach(function(e,i){
-        class2type[ "[object " + e + "]" ] = e.toLowerCase();
-    }) ;
-    function _typeof(obj){
-        if ( obj == null ){
-            return String( obj );
+    var class2type = {};
+    "Boolean Number String Function Array Date RegExp Object Error".split(" ").forEach(function(e, i) {
+        class2type["[object " + e + "]"] = e.toLowerCase();
+    });
+
+    function _typeof(obj) {
+        if (obj == null) {
+            return String(obj);
         }
         return typeof obj === "object" || typeof obj === "function" ?
-            class2type[ class2type.toString.call(obj) ] || "object" :
+            class2type[class2type.toString.call(obj)] || "object" :
             typeof obj;
     }
+
     function wtf_localStorage(name, value) {
         if (value) {
             localStorage.setItem(name, value);
@@ -120,97 +217,96 @@ margin-right:10px;
         }
     };
 
-    terminal.init=function(){
-        window.onkeydown=function(e){
-            if(e.keyCode===120||e.which===120){
+    terminal.init = function() {
+        window.onkeydown = function(e) {
+            if (e.keyCode === 120 || e.which === 120) {
                 terminal.show();
             }
         }
-        var terminalcon=document.createElement('div');
-        terminalcon.id='terminalcon';
-        terminalcon.innerHTML=csstemplate+template;
+        var terminalcon = document.createElement('div');
+        terminalcon.id = 'terminalcon';
+        terminalcon.innerHTML = navigator.userAgent.toLowerCase().indexOf('mobile') < 0 ? csstemplate + template : cssmobiletemplate + template;
         document.body.appendChild(terminalcon);
-        var terminal_TTYARR=JSON.parse(wtf_localStorage('terminal_TTYARR'));
-        var TTYARR=terminal_TTYARR?terminal_TTYARR.ttyarr:[];
-        var terminal_input=document.getElementById('terminal_input');
-        terminal_input.onkeydown=function(e){
-            if(e.keyCode===13||e.which===13){
-                var tty=terminal_input.value;
+        var terminal_TTYARR = JSON.parse(wtf_localStorage('terminal_TTYARR'));
+        var TTYARR = terminal_TTYARR ? terminal_TTYARR.ttyarr : [];
+        var terminal_input = document.getElementById('terminal_input');
+        terminal_input.onkeydown = function(e) {
+            if (e.keyCode === 13 || e.which === 13) {
+                var tty = terminal_input.value;
                 TTYARR.push(tty);
-                terminal_TTYARR={'ttyarr':TTYARR};
-                wtf_localStorage('terminal_TTYARR',JSON.stringify(terminal_TTYARR));
-                terminal_input.value='';
-                var terminal_show=document.getElementById('terminal_show');
-                terminal_show.innerHTML+='\n-'+tty+'\n';
-                try{
+                terminal_TTYARR = { 'ttyarr': TTYARR };
+                wtf_localStorage('terminal_TTYARR', JSON.stringify(terminal_TTYARR));
+                terminal_input.value = '';
+                var terminal_show = document.getElementById('terminal_show');
+                terminal_show.innerHTML += '\n-' + tty + '\n';
+                try {
                     terminal.log(terminal.eval(tty));
-                }catch(e){
+                } catch (e) {
                     terminal.log(e);
                 }
-                
-                terminal_show.scrollTop=terminal_show.scrollHeight;
+
+                terminal_show.scrollTop = terminal_show.scrollHeight;
             }
-            if(e.keyCode===38||e.which===38){
-                if(TTYARR.length>=1){
+            if (e.keyCode === 38 || e.which === 38) {
+                if (TTYARR.length >= 1) {
                     TTYARR.unshift(terminal_input.value);
-                    var tty=TTYARR.pop();
-                    terminal_input.value=tty;
+                    var tty = TTYARR.pop();
+                    terminal_input.value = tty;
                 }
             }
-            if(e.keyCode===40||e.which===40){
-                if(TTYARR.length>=1){
+            if (e.keyCode === 40 || e.which === 40) {
+                if (TTYARR.length >= 1) {
                     TTYARR.push(terminal_input.value);
-                    var tty=TTYARR.shift();
-                    terminal_input.value=tty;
+                    var tty = TTYARR.shift();
+                    terminal_input.value = tty;
                 }
             }
         }
     }
-    terminal.eval=function(tty){
-        var output=eval.call(window,tty);
+    terminal.eval = function(tty) {
+        var output = eval.call(window, tty);
         return output;
     }
-    terminal.alert=function(text, hidetime) {
+    terminal.alert = function(text, hidetime) {
         var hidetime = hidetime || 3000;
         if (terminal_alerttimer) {
             clearTimeout(terminal_alerttimer);
         }
-        var showalerthtml = '<div style="position:fixed;left:50%;top:45%;font-size:20px;color:white;background:black;border-radius:5px;padding:10px;opacity:0.8;">' + text + '</div>';
-        if (document.getElementById('showalert')) {
-            document.getElementById('showalert').innerHTML = showalerthtml;
+        var terminal_alertconhtml = '<pre class="terminal_alert">' + text + '</pre>';
+        if (document.getElementById('terminal_alertcon')) {
+            document.getElementById('terminal_alertcon').innerHTML = terminal_alertconhtml;
         } else {
-            var showalert = document.createElement('div');
-            showalert.id = 'showalert';
-            showalert.innerHTML = showalerthtml;
-            document.body.appendChild(showalert);
+            var terminal_alertcon = document.createElement('div');
+            terminal_alertcon.id = 'terminal_alertcon';
+            terminal_alertcon.innerHTML = terminal_alertconhtml;
+            document.body.appendChild(terminal_alertcon);
         }
         terminal_alerttimer = setTimeout(function() {
-            document.getElementById('showalert').innerHTML = '';
+            document.getElementById('terminal_alertcon').innerHTML = '';
         }, hidetime);
     }
-    terminal.show=function(){
-        var terminal=document.getElementById('terminal');
-        if(terminal.style.display=='none'){
-            terminal.style.display='block';
+    terminal.show = function() {
+        var terminal = document.getElementById('terminal');
+        if (terminal.style.display == 'none') {
+            terminal.style.display = 'block';
             document.getElementById('terminal_input').focus();
-        }else{
-            terminal.style.display='none';
+        } else {
+            terminal.style.display = 'none';
         }
     }
-    
-    terminal.log=function(){
-        var output='';
-        for(var i=0;i<arguments.length;i++){
-            if(_typeof(arguments[i])==='object'){
-                output+=JSON.stringify(arguments[i],0,4)+'\n';
-            }
-            else{
-                output+=arguments[i]+' ';
+
+    terminal.log = function() {
+        var output = '';
+        for (var i = 0; i < arguments.length; i++) {
+            if (_typeof(arguments[i]) === 'object') {
+                output += JSON.stringify(arguments[i], 0, 4) + '\n';
+            } else {
+                output += arguments[i] + ' ';
             }
         }
         //console.log('terminal: '+output);
-        var show=document.getElementById('terminal_show');
-        show.innerHTML+=output+'\n';
+        var show = document.getElementById('terminal_show');
+        show.innerHTML += output + '\n';
     }
     return terminal;
 })()
